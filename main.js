aacf8e7b63c790077a5537f603b040964bf4d100
Lissana <liss.glace471@gmail.com>
2022-04-13 18:54:15 +0900

    feat: オプションの指定(gfm)を追加
    ・デフォルトはgfm:false

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const program = require("commander");
 const fs = require("fs");
-// markedモジュールをmarkedオブジェクトとしてインポートする
 const marked = require("marked");
 
+// gfmオプションを定義する
+program.option("--gfm", "GFMを有効にする");
 program.parse(process.argv);
 const filePath = program.args[0];
+
+// コマンドライン引数のオプションを取得する
+const options = program.opts();
+
+// コマンドライン引数で指定されなかったオプションにデフォルト値を上書きする
+const cliOptions = {
+    gfm: options.gfm ?? false,
+};
 
 fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
     if (err) {
@@ -12,7 +21,9 @@ fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
         process.exit(1);
         return;
     }
-    // MarkdownファイルをHTML文字列に変換する
-    const html = marked.parse(file);
+    const html = marked.parse(file, {
+        // オプションの値を使用する
+        gfm: cliOptions.gfm,
+    });
     console.log(html);
 });
