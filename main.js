dd793a8cb66a13f6e13d0222212f5e62c901c435
Lissana <liss.glace471@gmail.com>
2022-04-13 17:01:43 +0900

    feat: コマンドライン引数で渡されたファイルパス（文字列）の取得機能を追加
    ・commanderを使ってコマンドライン引数をパース
    ・渡された文字列をコンソールに出力

---
+++ main.js
@@ -0,0 +1,9 @@
+// commanderモジュールをprogramとしてインポートする
+const program = require("commander");
+// コマンドライン引数をcommanderでパースする
+program.parse(process.argv);
+
+// ファイルパスをprogram.args配列から取り出す
+const filePath = program.args[0];
+console.log(filePath);
+console.log(program.args);
