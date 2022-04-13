33e79cd2ba6e8a20c2e7fe7f3ae67e8156023095
Lissana <liss.glace471@gmail.com>
2022-04-13 17:13:39 +0900

    feat: ファイル読み込み処理の追加
    ・readFile関数で取得したファイルパスの読み込みコンソール出力
    ・エラーハンドリングの追加

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 // commanderモジュールをprogramとしてインポートする
 const program = require("commander");
-// コマンドライン引数をcommanderでパースする
-program.parse(process.argv);
+// fsモジュールをfsオブジェクトとしてインポートする
+const fs = require("fs");
 
 // ファイルパスをprogram.args配列から取り出す
+program.parse(process.argv);
 const filePath = program.args[0];
-console.log(filePath);
-console.log(program.args);
+
+// ファイルをutf-8として非同期で読み込む
+fs.readFile(filePath, {encoding: "utf-8"}, (err, file) =>{
+    if(err){
+        console.error(err.message);
+        // 終了ステータス 1（一般的なエラー）としてプロセスを終了する
+        process.exit(1);
+        return;
+    }else{
+        console.log(file);
+    }
+})
